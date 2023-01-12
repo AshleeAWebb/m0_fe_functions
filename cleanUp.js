ef4beedde1596154b34b96aaf4df5ed0dfc8394c
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-12 14:25:58 -0500

    Update cleanUp

--- cleanUp.js
+++ cleanUp.js
@@ -13,7 +13,7 @@ function nameQuestion() {
 }
 
 nameQuestion()
-//Added semicolon to line 12 after the code block.
+//Added semicolon to line 12 after the code block as it is the industry standard.
 
 // EX 2:
 function addThreeNums(first, second, third) {
@@ -23,7 +23,8 @@ console.log(sum);
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-// Line 21 lined up curly bracket with function on line 19. Added semicolon end on line 20.
+// Line 21 lined up curly bracket with function on line 19 to display that this is the end of this code block.
+// Added semicolon end on line 20 industry standard.
 
 // EX 3:
 function makeFreshPesto() {
@@ -37,7 +38,8 @@ function makeFreshPesto() {
 makeFreshPesto();
 
 //Line 29 completed phrase function and added a space between parenthesis and curly brackets. 
-//Line 34 dropped curly bracket to next line and alinged with funtion.
+//This allowed to function to be declared properly and formatted the line up. 
+//Line 34 dropped curly bracket to next line and alinged with function.
 
 //  EX 4:
 function average(num1, num2) {
